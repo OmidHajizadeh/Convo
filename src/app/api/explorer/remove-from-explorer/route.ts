28b3aaad9e7949aab647bb289564b84ec942744c
Omid Hajizadeh <omid.hajizadehh@gmail.com>
2023-09-02 22:00:14 +0330

    github pages changes made 7

--- src/app/api/explorer/remove-from-explorer/route.ts
+++ src/app/api/explorer/remove-from-explorer/route.ts
@@ -3,7 +3,7 @@ import { fetchRedis } from "@/utils/fetchRedis";
 import { fetchServerSession } from "@/utils/serverInteractions";
 import { NextResponse } from "next/server";
 
-export const dynamic = 'force-static';
+export const revalidate = 0;
 
 export async function GET() {
   const session = await fetchServerSession();
